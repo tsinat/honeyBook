c7b233230530cffd8245916f479faca1db7d5c6b
Tsinat Zeree <cheru87@gmail.com>
2016-12-12 23:20:14 -0800

    working spinning loader and animation

--- js/controllers/controller.js
+++ js/controllers/controller.js
@@ -2,11 +2,13 @@
 
 var app = angular.module('honeyBook');
 
-app.controller('mainCtrl', function($scope, User)  {
+app.controller('mainCtrl', function($scope, User, $timeout)  {
     User.getUser()
         .then(response => {
             console.log(response.data);
-            $scope.contactList = response.data;
+            $timeout(function(){
+                $scope.contactList = response.data;
+            },6000)
         })
         .catch(err =>  {
             console.log('error while getting contactList:', err);
--- js/services/service.js
+++ js/services/service.js
@@ -2,6 +2,6 @@ var app = angular.module('honeyBook');
 
 app.service('User', function($http) {
     this.getUser = () => {
-        return $http.get('https://candidate-test.herokuapp.com/contacts');
+        return $http.get('https://candidate-test.herokuapp.com/contacts', { cache: true});
     }
 });
